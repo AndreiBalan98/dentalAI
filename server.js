048c0589ae925fe661902e20630c08043a7488da
Andrei Balan <andrei.balan98@gmail.com>
2025-07-15 01:59:27 +0300

    working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express       = require('express');
 const voiceRoutes   = require('./routes/voice');
 const smsRoutes     = require('./routes/sms');
-require('dotenv').config();
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
